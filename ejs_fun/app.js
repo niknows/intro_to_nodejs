62cf38b627d84593a68a790b109a7bbebb1ced70
niknows <33270711+niknows@users.noreply.github.com>
2018-07-02 21:50:42 -0300

    Create app.js

---
+++ ejs_fun/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/',function(req, res){
+    res.render('home.ejs');
+    
+});
+app.get("/fallinginlovewith/:thing",function(req,res){
+    var thing = req.params.thing;
+    res.render("love.ejs",{thingVar: thing});
+});
+app.get("/posts",function(req,res){
+    var posts = [{title: "Post 1",author: "Suzy"},
+                 {title: "Second Post",author: "Nik"},
+                 {title: "Great News!",author: "Suzy"}
+        ];
+        res.render("posts.ejs", {posts: posts});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The server has started");
+});
