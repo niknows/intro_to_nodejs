9ff6e8a126248e3e0851d773e29d8200b4701a5c
niknows <33270711+niknows@users.noreply.github.com>
2018-07-03 22:23:41 -0300

    Create app.js

---
+++ ejs-partials-exercise/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get('/',function(req, res){
+    res.render('home');
+    
+});
+app.get("/fallinginlovewith/:thing",function(req,res){
+    var thing = req.params.thing;
+    res.render("love",{thingVar: thing});
+});
+app.get("/posts",function(req,res){
+    var posts = [{title: "Post 1",author: "Suzy"},
+                 {title: "Second Post",author: "Nik"},
+                 {title: "Great News!",author: "Suzy"}
+        ];
+        res.render("posts", {posts: posts});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The server has started");
+});
